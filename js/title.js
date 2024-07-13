44e40e822496f040fdcc0d2c9bc268621c3c5578
ikun123234 <3167722168@qq.com>
2024-07-13 23:45:19 +0800

    Site updated: 2024-07-13 23:45:18

--- js/title.js
+++ js/title.js
@@ -1,13 +1,1 @@
-var OriginTitile = document.title; 
-var titleTime; 
-document.addEventListener('visibilitychange', function () { 
-    if (document.hidden) { 
-        document.title = ' 不要走~'; 
-        clearTimeout(titleTime); 
-    } else { 
-        document.title = ' 欢迎回来～'; 
-        titleTime = setTimeout(function () { 
-            document.title = OriginTitile; 
-        }, 2000); 
-    } 
-});
+"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title=" 不要走~",clearTimeout(titleTime)):(document.title=" 欢迎回来～",titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
